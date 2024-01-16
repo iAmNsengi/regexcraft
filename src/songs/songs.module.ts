ca6bb2b620fb92576ea95cdbd23ddf3e3753a16f
Eliezer Nsengi <nsengitech@gmail.com>
2024-01-16 21:18:26 +0200

    chore: project reset

--- src/songs/songs.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SongsService } from './songs.service';
-import { SongsController } from './songs.controller';
-
-@Module({
-  providers: [SongsService],
-  controllers: [SongsController]
-})
-export class SongsModule {}
