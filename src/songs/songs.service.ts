08f867da0db0543cb916c912c6d93d77e7dc2666
Eliezer Nsengi <nsengitech@gmail.com>
2024-11-18 08:16:53 +0200

    add: song service

--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class SongsService {}
+export class SongsService {
+  private readonly songs = [];
+  findAll() {
+    return this.songs;
+  }
+  createSong(song) {
+    this.songs.push(song);
+    return this.songs;
+  }
+}
