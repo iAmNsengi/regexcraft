733c92e4c9b54b920da2d92ed49b97733ec69f58
Eliezer Nsengi <nsengitech@gmail.com>
2024-11-29 22:09:14 +0200

    add: initial presets

--- src/index.js
+++ src/index.js
@@ -20,6 +20,40 @@ class RegexCraft {
             message: "At least one number",
           },
         ],
+        medium: [
+          { pattern: "(?=.{8,})", message: "At least 8 characters" },
+          {
+            pattern: "(?=.*\\d)",
+            message: "At least one number",
+          },
+          {
+            pattern: "(?=.*[a-z])",
+            message: "At least one lowercase letter",
+          },
+          {
+            pattern: "(?=.*[A-Z])",
+            message: "At least one uppercase letter",
+          },
+        ],
+        high: [
+          { pattern: "(?=.{10,})", message: "At least 8 characters" },
+          {
+            pattern: "(?=.*\\d{2,0})",
+            message: "At least two number",
+          },
+          {
+            pattern: "(?=.*[a-z])",
+            message: "At least one lowercase letter",
+          },
+          {
+            pattern: "(?=.*[A-Z])",
+            message: "At least one uppercase letter",
+          },
+          {
+            pattern: "(?=.*[!@#$%^&*])",
+            message: "At least one special character",
+          },
+        ],
       },
     };
   }
