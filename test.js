65ee791def61a0e0c84d483c57a403faf5ff3807
Eliezer Nsengi <79418999+iAmNsengi@users.noreply.github.com>
2024-11-30 09:51:38 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,5 +3,19 @@ import RegexCraft from "./src/index.js";
 const passwordValidator = new RegexCraft().usePreset("password", "low");
 //   .hasLengthBetween(2, 8);
 
-console.log(passwordValidator.test(["nsengi1988888", "trsting"]));
+console.log(passwordValidator.test(["nsengi1988888"]));
+console.log(passwordValidator.test(["nsengi1988888"]));
 console.log(passwordValidator.test(["my password"]).isValid);
+
+const lengthValidator = new RegexCraft()
+  .hasLengthBetween(2, 4)
+  .hasExactLength(3);
+
+console.log(lengthValidator.test(["123333"]));
+
+const characterValidator = new RegexCraft()
+  .hasLetter(2)
+  .hasUpperCase("2")
+  .hasNumber(2)
+  .hasSpecialCharacter(5);
+console.log(characterValidator.test(["123@^4AR4"]));
